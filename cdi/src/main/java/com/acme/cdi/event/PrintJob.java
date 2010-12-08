042fb2a485069d48885f67a2da6300c30ede2119
Dan Allen <dan.j.allen@gmail.com>
2010-12-07 19:39:23 -0500

    fix compile error

--- cdi/src/main/java/com/acme/cdi/event/PrintJob.java
+++ cdi/src/main/java/com/acme/cdi/event/PrintJob.java
@@ -1,7 +1,9 @@
 package com.acme.cdi.event;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 import javax.inject.Qualifier;
 
