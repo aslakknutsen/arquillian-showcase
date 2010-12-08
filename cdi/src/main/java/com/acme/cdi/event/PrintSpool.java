8bd9ddcac93c959576d1d8a4726c581d1b27f973
Dan Allen <dan.j.allen@gmail.com>
2010-12-07 19:55:23 -0500

    add documentation for observer resolution rules

--- cdi/src/main/java/com/acme/cdi/event/PrintSpool.java
+++ cdi/src/main/java/com/acme/cdi/event/PrintSpool.java
@@ -10,6 +10,45 @@ import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.Default;
 
+/**
+ * A hypothetical print spool that demonstrates the selection and invocation of
+ * event observers.
+ * 
+ * <p>
+ * This bean receives document events and records the document as printed. It
+ * classifies received documents by job size.
+ * </p>
+ * 
+ * <p>
+ * Observer resolution is defined in section 10.2 of the JSR-299 specification.
+ * Type-safe resolution rules, defined in section 5.2 of the JSR-299
+ * specification, are used when resolving observers for an event.
+ * </p>
+ * 
+ * <p>
+ * An observer method will be notified of an event if the event object is
+ * assignable to the observed event type, and if all the qualifiers of the event
+ * observer are also qualifiers of the event.
+ * </p>
+ * 
+ * <p>
+ * Thus, an event must have all of the qualifiers specified on the observer
+ * (called the required qualifiers) for the observer to match the event. The
+ * event may have additional qualifiers not specified on the observer. In other
+ * words, the qualifiers on the observer must be a subset of the qualifiers on
+ * the event (or the qualifiers on the event must be a superset of the
+ * qualifiers on the observer).
+ * </p>
+ * 
+ * <p>
+ * If the observer does not specify any qualifiers, it's assigned the &#064;Any
+ * qualifier, and hence matches an event with any number of qualifiers. An
+ * observer with the qualifier &#064;Default matches an event with no qualifiers
+ * since events with no qualifiers are assigned the &#064;Default qualifier.
+ * </p>
+ * 
+ * @author <a href="http://community.jboss.org/people/dan.j.allen">Dan Allen</a>
+ */
 @RequestScoped
 public class PrintSpool
 {
