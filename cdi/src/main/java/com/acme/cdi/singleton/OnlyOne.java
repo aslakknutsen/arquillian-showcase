359f1a8d146e412bb6d7151a92c322c16368c57d
Dan Allen <dan.j.allen@gmail.com>
2011-04-12 08:08:30 +0100

    add example for singleton

---
+++ cdi/src/main/java/com/acme/cdi/singleton/OnlyOne.java
@@ -0,0 +1,21 @@
+package com.acme.cdi.singleton;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import javax.annotation.PostConstruct;
+
+@javax.inject.Singleton
+public class OnlyOne {
+    public static AtomicLong seed = new AtomicLong(0);
+    
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+    
+    @PostConstruct
+    public void assignId() {
+        id = seed.incrementAndGet();
+    }
+}
